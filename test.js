e30c2c5cbf051345847213fdd88a78e1d5bfb3e4
Bapi Das <bapidas777@users.noreply.github.com>
2026-07-03 07:05:27 +0530

    Final polish and architectural refinements

---
+++ test.js
@@ -0,0 +1,4 @@
+const sdk = require('@creit.tech/stellar-wallets-kit');
+console.log(Object.keys(sdk));
+const utils = require('@creit.tech/stellar-wallets-kit/script/sdk/modules/utils.js');
+console.log(Object.keys(utils));
---
+++ test2.js
@@ -0,0 +1,3 @@
+const sdk = require('@creit.tech/stellar-wallets-kit');
+const f = require('@creit.tech/stellar-wallets-kit/modules/freighter');
+console.log(Object.keys(f));
