f293455f8a1307ba057f3bc28b09a5fe860838a7
Bapi Das <bapidas777@users.noreply.github.com>
2026-07-04 22:38:21 +0530

    chore: Clean up scratchpad scripts and system files

--- test.js
+++
@@ -1,4 +0,0 @@
-const sdk = require('@creit.tech/stellar-wallets-kit');
-console.log(Object.keys(sdk));
-const utils = require('@creit.tech/stellar-wallets-kit/script/sdk/modules/utils.js');
-console.log(Object.keys(utils));
--- test2.js
+++
@@ -1,3 +0,0 @@
-const sdk = require('@creit.tech/stellar-wallets-kit');
-const f = require('@creit.tech/stellar-wallets-kit/modules/freighter');
-console.log(Object.keys(f));
